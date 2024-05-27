77cb1b8a61be072a989441da3e79829774fa5535
unknown <cynthiamedina18.08@gmail.com>
2024-05-27 18:09:28 -0300

    modificando media query para celular

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import styles from './FooterStyles.module.css'
+import logoCynMedina from '../../../public/logoCynMedina.png';
 
 function Footer() {
     return (
         <section id="footer" className={styles.container}>
-            <img src="../../../public/logoCynMedina.png" alt="logo full satck developer Cynthia Medina" />
+            <img src={logoCynMedina} alt="logo full satck developer Cynthia Medina" />
             <p>&copy; 2024 Cynthia Medina
                 <br />
                 Todos los derechos reservados
