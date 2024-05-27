b2d5b530c91f7927c2b1b85415d127894ab943a5
unknown <cynthiamedina18.08@gmail.com>
2024-05-27 16:04:19 -0300

    logo Cynthia

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css'
 function Footer() {
     return (
         <section id="footer" className={styles.container}>
-            <img src="../../../public/logoCynMedina.png" alt="" />
+            <img src="../../../public/logoCynMedina.png" alt="logo full satck developer Cynthia Medina" />
             <p>&copy; 2024 Cynthia Medina
                 <br />
                 Todos los derechos reservados
